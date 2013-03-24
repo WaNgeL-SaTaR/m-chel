64c0f1408eb469b936cd229aaeadfcde497b4dcc
Taras <tapacpro@gmail.com>
2013-03-24 12:52:26 +0600

    final menu js completed!!!

--- js/my_sammy.js
+++ js/my_sammy.js
@@ -449,14 +449,17 @@ Handlebars.registerHelper('attachNames', function(items) {
 		                .replace('#main')
 		                .then(function () {
 
-$(document).ready(function () {
-		$('.triple-div').removeClass('active-menu');
-	        $('#menu_' + page).addClass('active-menu');
+				    $(document).ready(function () {
+					$(".collapse").collapse({toggle: false});
 
+					$('.triple-div').removeClass('active-menu');
+					$('#menu_' + page).addClass('active-menu');
+					$('h5').next().collapse('hide');
 
-		$('.active-menu:contains(Черный металлопрокат)').each(function(){$('h5:contains(Черный металлопрокат)').next().collapse('show')});
-		$('.active-menu:contains(Нержавеющий металлопрокат)').each(function(){$('h5:contains(Нержавеющий металлопрокат)').next().collapse('show')});
-		$('.active-menu:contains(Цветной металлопрокат)').each(function(){$('h5:contains(Цветной металлопрокат)').next().collapse('show')});
+					$('.active-menu:contains(Черный металлопрокат)').each(function(){$('h5:contains(Черный металлопрокат)').next().collapse('show')});
+					$('.active-menu:contains(Нержавеющий металлопрокат)').each(function(){$('h5:contains(Нержавеющий металлопрокат)').next().collapse('show')});
+					$('.active-menu:contains(Цветной металлопрокат)').each(function(){$('h5:contains(Цветной металлопрокат)').next().collapse('show')});
+
 
 });
 
